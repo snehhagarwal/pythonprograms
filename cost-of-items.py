d9dd53de45c7a53c9fc5eca590ccc0195d4897ac
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-25 01:25:11 +0530

    Add files via upload

---
+++ cost-of-items.py
@@ -0,0 +1,7 @@
+n=int(input("no. of items:"))
+if n<10:
+     print("the total cost is:",n*120)
+elif n>10 and n<100:
+    print("the total cost is",n*100)
+elif n>100 :
+    print("the total cost is :",n*70)
