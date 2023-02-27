070df704b095efe1e12fec635418ce94336d3695
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-28 01:19:29 +0530

    Add files via upload

---
+++ avg temp of week.py
@@ -0,0 +1,9 @@
+a=int(input("the temperature on monday was recorded as:"))
+b=int(input("the temperature on tuesday was recorded as:"))
+c=int(input("the temperature on wednesday was recorded as:"))
+d=int(input("the temperature on thursday was recorded as:"))
+e=int(input("the temperature on friday was recorded as:"))
+f=int(input("the temperature on saturday was recorded as:"))
+g=int(input("the temperature on sunday was recorded as:"))
+avg=(a+b+c+d+e+f+g)/7
+print("the average temperature of the week is:",avg)
