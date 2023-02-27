a89e84970691488e207e73b57e77afebdb33f5c8
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2023-02-28 01:21:35 +0530

    Add files via upload

---
+++ divisibility of a number.py
@@ -0,0 +1,8 @@
+print("question 6")
+a=int(input("the first number is:"))
+b=int(input("the second number is:"))
+r=a%b
+if r==0:
+   print(a,"is divisible by",b)
+else:
+   print(a,"is not divisible by",b)
